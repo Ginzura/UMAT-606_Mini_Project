2a5b11c6f733f4758e6861687d84495c40bf5b81
Gin_zura <110963116+Ginzura@users.noreply.github.com>
2024-04-05 14:22:26 +0530

    delete

--- MINI_PROJ/REPL/word/header_repl_c.h
+++
@@ -1,7 +0,0 @@
-#ifndef HEADER_REPL_C_H
-#define HEADER_REPL_C_H
-
-// Function declaration
-void replaceAll(char *str, const char *oldWord, const char *newWord);
-
-#endif /* HEADER_REPL_C_H */
--- Makefile
+++
@@ -1,9 +0,0 @@
-srep: main.o functions.o
-	gcc main.o functions.o -o srep
-	
-main.o: main.c
-	gcc -c main.c
-	
-functions.o: functions.c
-	gcc -c functions.c
-	
